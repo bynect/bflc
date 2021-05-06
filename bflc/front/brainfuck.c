788dd56a952e73f1f2902b08cb206374a9ab0961
bynect <68197565+bynect@users.noreply.github.com>
2021-05-07 00:27:19 +0200

    Add Brainfuck frontend implementation

--- bflc/front/brainfuck.c
+++ bflc/front/brainfuck.c
@@ -4,5 +4,103 @@ error_t
 scan_brainfuck(const char *src, size_t len, ir_t *ir)
 {
     error_t error = {NULL, NULL};
+
+    uint32_t line = 1;
+    uint32_t column = 1;
+    size_t instr = 0;
+
+    for (size_t i = 0; i < len; ++i)
+    {
+        ++column;
+        switch (src[i])
+        {
+            case '>':
+                ir_ensure(ir, instr);
+                ir->instrs[instr].instr = INSTR_PTRINC;
+                ir->instrs[instr].arg = 1;
+                ir->instrs[instr].line = line;
+                ir->instrs[instr].column = column;
+                ir->instrs[instr].offset = i;
+                ++instr;
+                break;
+
+            case '<':
+                ir_ensure(ir, instr);
+                ir->instrs[instr].instr = INSTR_PTRDEC;
+                ir->instrs[instr].arg = 1;
+                ir->instrs[instr].line = line;
+                ir->instrs[instr].column = column;
+                ir->instrs[instr].offset = i;
+                ++instr;
+                break;
+
+            case '+':
+                ir_ensure(ir, instr);
+                ir->instrs[instr].instr = INSTR_CELINC;
+                ir->instrs[instr].arg = 1;
+                ir->instrs[instr].line = line;
+                ir->instrs[instr].column = column;
+                ir->instrs[instr].offset = i;
+                ++instr;
+                break;
+
+            case '-':
+                ir_ensure(ir, instr);
+                ir->instrs[instr].instr = INSTR_CELDEC;
+                ir->instrs[instr].arg = 1;
+                ir->instrs[instr].line = line;
+                ir->instrs[instr].column = column;
+                ir->instrs[instr].offset = i;
+                ++instr;
+                break;
+
+            case '.':
+                ir_ensure(ir, instr);
+                ir->instrs[instr].instr = INSTR_OUTPUT;
+                ir->instrs[instr].arg = -1;
+                ir->instrs[instr].line = line;
+                ir->instrs[instr].column = column;
+                ir->instrs[instr].offset = i;
+                ++instr;
+                break;
+
+            case ',':
+                ir_ensure(ir, instr);
+                ir->instrs[instr].instr = INSTR_INPUT;
+                ir->instrs[instr].arg = -1;
+                ir->instrs[instr].line = line;
+                ir->instrs[instr].column = column;
+                ir->instrs[instr].offset = i;
+                ++instr;
+                break;
+
+            case '[':
+                ir_ensure(ir, instr);
+                ir->instrs[instr].instr = INSTR_JMPBEG;
+                ir->instrs[instr].arg = -1;
+                ir->instrs[instr].line = line;
+                ir->instrs[instr].column = column;
+                ir->instrs[instr].offset = i;
+                ++instr;
+                break;
+
+            case ']':
+                ir_ensure(ir, instr);
+                ir->instrs[instr].instr = INSTR_JMPEND;
+                ir->instrs[instr].arg = -1;
+                ir->instrs[instr].line = line;
+                ir->instrs[instr].column = column;
+                ir->instrs[instr].offset = i;
+                ++instr;
+                break;
+
+            case '\n':
+                column = 1;
+                ++line;
+                break;
+        }
+    }
+
+    ir->end = instr;
     return error;
 }
