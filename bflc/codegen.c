4877649ec4b2acd97d98f0379c67f4997c653b04
bynect <68197565+bynect@users.noreply.github.com>
2021-05-07 14:30:45 +0200

    Add abstracted codegen implementation

---
+++ bflc/codegen.c
@@ -0,0 +1,59 @@
+#include "codegen.h"
+
+error_t
+codegen_run(context_t *ctx, codegen_t *codegen, ir_t *ir)
+{
+    error_t err;
+    error_init(&err, NULL, NULL);
+
+    bytebuffer_t buf;
+    bytebuffer_init(&buf, BYTEBUFFER_BLOCK);
+
+    codegen->prologue_fn(ctx, &buf, &err, codegen->extra);
+
+    for (size_t i = 0; i < ir->end; ++i)
+    {
+        switch (ir->instrs[i].instr)
+        {
+            case INSTR_PTRINC:
+                codegen->ptrinc_fn(ctx, &buf, &err, codegen->extra);
+                break;
+
+            case INSTR_PTRDEC:
+                codegen->ptrdec_fn(ctx, &buf, &err, codegen->extra);
+                break;
+
+            case INSTR_CELINC:
+                codegen->celinc_fn(ctx, &buf, &err, codegen->extra);
+                break;
+
+            case INSTR_CELDEC:
+                codegen->celdec_fn(ctx, &buf, &err, codegen->extra);
+                break;
+
+            case INSTR_OUTPUT:
+                codegen->output_fn(ctx, &buf, &err, codegen->extra);
+                break;
+
+            case INSTR_INPUT:
+                codegen->input_fn(ctx, &buf, &err, codegen->extra);
+                break;
+
+            case INSTR_JMPBEG:
+                codegen->jmpbeg_fn(ctx, &buf, &err, codegen->extra);
+                break;
+
+            case INSTR_JMPEND:
+                codegen->jmpend_fn(ctx, &buf, &err, codegen->extra);
+                break;
+
+            default:
+                error_node(&err, "Invalid instruction", ir->instrs + i);
+                break;
+        }
+    }
+
+    codegen->epilogue_fn(ctx, &buf, &err, codegen->extra);
+
+    return err;
+}
---
+++ bflc/codegen.h
@@ -0,0 +1,29 @@
+#ifndef BFLC_CODEGEN_H
+#define BFLC_CODEGEN_H
+
+#include "bytebuffer.h"
+#include "ir.h"
+#include "error.h"
+#include "context.h"
+
+typedef bool (*codegen_fn)(
+    context_t *ctx, bytebuffer_t *buf, error_t *err, void *extra
+);
+
+typedef struct {
+    codegen_fn prologue_fn;
+    codegen_fn epilogue_fn;
+    codegen_fn ptrinc_fn;
+    codegen_fn ptrdec_fn;
+    codegen_fn celinc_fn;
+    codegen_fn celdec_fn;
+    codegen_fn output_fn;
+    codegen_fn input_fn;
+    codegen_fn jmpbeg_fn;
+    codegen_fn jmpend_fn;
+    void *extra;
+} codegen_t;
+
+error_t codegen_run(context_t *ctx, codegen_t *codegen, ir_t *ir);
+
+#endif
