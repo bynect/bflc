379d7a62b731a3f7997aee4e303d8146bf128e67
bynect <68197565+bynect@users.noreply.github.com>
2021-06-03 00:46:48 +0200

    Add jit_example.c implementation

--- docs/jit_example.c
+++ docs/jit_example.c
@@ -1,1 +1,121 @@
-//TODO
+#include "front/brainfuck.h"
+#include "mid/validation.h"
+#include "mid/folding.h"
+#include "back/x86_64.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int
+jit_execute(bytebuffer_t *buf)
+{
+    int fd = open("/dev/zero", O_RDWR);
+    void *exc = mmap(
+        0, buf->pos, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE, fd, 0
+    );
+
+    if (exc == MAP_FAILED)
+    {
+        perror("mmap");
+        return 1;
+    }
+
+    printf("Before JIT execution.\n");
+    printf("----------------------------------------\n");
+
+    memcpy(exc, buf->bytes, buf->pos);
+    void (*fn)() = exc;
+    fn();
+
+    printf("----------------------------------------\n");
+    printf("After JIT execution.\n");
+
+    munmap(exc, buf->pos);
+    close(fd);
+    return 0;
+}
+
+#define CHECK_ERR(err, ir, msg) \
+    if (error_dump(&err) > 0) \
+    { \
+        error_free(&err); \
+        ir_free(&ir); \
+        return 1; \
+    } \
+    else \
+    { \
+        printf(msg); \
+    }
+
+int
+main(int argc, const char **argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: %s file\n", argv[0]);
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "rb");
+
+    if (file == NULL)
+    {
+        perror("fopen");
+        return 1;
+    }
+
+    fseek(file, 0, SEEK_END);
+    size_t size = ftell(file);
+    rewind(file);
+
+    char *src = malloc(size + 1);
+    src[size] = '\0';
+
+    fread(src, 1, size, file);
+    fclose(file);
+
+    ir_t ir;
+    ir_init(&ir);
+
+    error_t err = scan_brainfuck(src, size, &ir);
+    CHECK_ERR(err, ir, "Program scanned\n");
+
+    context_t *ctx = context_new();
+    size_t cells = 30000;
+    context_set(ctx, CTX_CELLS, &cells);
+
+    bool syscall = true;
+    context_set(ctx, CTX_FWRITE, &syscall);
+    context_set(ctx, CTX_FREAD, &syscall);
+
+    err = pass_validation(ctx, &ir);
+    CHECK_ERR(err, ir, "Program validated\n");
+
+    err = pass_folding(ctx, &ir);
+    CHECK_ERR(err, ir, "Program optimized\n");
+
+    bytebuffer_t buf;
+    bytebuffer_init(&buf, BYTEBUFFER_BLOCK);
+
+    err = emit_mach_x86_64(ctx, &buf, &ir);
+
+    if (!error_success(&err))
+    {
+        bytebuffer_free(&buf);
+    }
+
+    CHECK_ERR(err, ir, "Program compiled\n");
+    int res = jit_execute(&buf);
+
+    bytebuffer_free(&buf);
+    ir_free(&ir);
+    context_free(ctx);
+    return res;
+}
